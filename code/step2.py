5a903d5c7c3d5f68acdb6571aff56b9ee0a2c06a
peano4783 <peternovikov@yandex.ru>
2023-03-31 14:08:47 +0300

    Slightly modified step2

--- code/step2.py
+++ code/step2.py
@@ -27,9 +27,11 @@ def index_page():
     hist = hv.Histogram((edges, frequencies)).opts(width=500, height=300, tools=['hover'])
 
     # Distilling the HoloViews objects scatter and hist into Bokeh objects and
+    bokeh_scatter, bokeh_hist = hv.render(scatter), hv.render(hist)
+
     # generating JavaScript and HTML for the Bokeh objects
-    scatter_script, scatter_div = bokeh.embed.components(hv.render(scatter))
-    hist_script, hist_div = bokeh.embed.components(hv.render(hist))
+    scatter_script, scatter_div = bokeh.embed.components(bokeh_scatter)
+    hist_script, hist_div = bokeh.embed.components(bokeh_hist)
 
     # Using step2_index.html template to generate an actual HTML page.
     # JavaScript and HTML of the graphs are passed to the template among other parameters.
